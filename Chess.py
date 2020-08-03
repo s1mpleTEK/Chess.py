d1dd14b5d28d3e27b52c3dccc46b291c910c185e
s1mpleTEK <57884979+s1mpleTEK@users.noreply.github.com>
2020-08-03 22:41:18 +0200

    Added print of the board

--- Chess.py
+++ Chess.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
 
+import numpy as np
+
+def set_table():
+    table = np.zeros((8,8), dtype=str)
+    for i in range (8):
+        for j in range (8):
+            table[i][j] = '.'
+    return (table)
+
+def display_table(table):
+    for i in range (8):
+        print(table[i])
+
 if __name__ == "__main__":
-    print("Hello word")
+    try:
+        print("Hello word")
+        table = set_table()
+        display_table(table)
+    except (EOFError, KeyboardInterrupt) as error:
+        exit()
