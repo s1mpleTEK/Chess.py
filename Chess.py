2c57d1805db4f35fbbc28b137ba4afadefa280ed
s1mpleTEK <57884979+s1mpleTEK@users.noreply.github.com>
2020-08-04 01:12:26 +0200

    Added setup of full chess board without pieces

--- Chess.py
+++ Chess.py
@@ -2,21 +2,29 @@
 
 import numpy as np
 
-def set_table():
-    table = np.zeros((8,8), dtype=str)
-    for i in range (8):
-        for j in range (8):
-            table[i][j] = '.'
+def set_table():                                    #init game board
+    table = np.zeros((10,10), dtype=str)            #init empty list 10x10 ([9][9])
+    for i in range (1,9):                           #set * in every line
+        table[i] = '*'
+
+    for i in range (2):                             #i=0 > i=1
+        table[i*9] = "¤"                            #set ¤ in every corner and browse y of table (first line and last line)
+        for j in range (8):                         #browse x of table
+            table[i*9][j+1] = chr(ord("A")+j)       #A>B>C>D>E>F>G>H
+
+    for i in range (2):                             #browse x of table (first column and last column)
+        for j in range (1,9):                       #browse y of table and set the number 8 to 1 at [1][0] to [8][0] and [1][9] to [8][9]
+            table[j][i*9] = 9-j                     #1>2>3>4>5>6>7>8
+
     return (table)
 
-def display_table(table):
-    for i in range (8):
+def display_table(table):       #display game board
+    for i in range (10):
         print(table[i])
 
 if __name__ == "__main__":
     try:
-        print("Hello word")
-        table = set_table()
-        display_table(table)
+        table = set_table()     #init game board
+        display_table(table)    #display game board
     except (EOFError, KeyboardInterrupt) as error:
         exit()
