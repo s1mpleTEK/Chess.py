7c8e3dad7f3a68868e2163038db27a8a03529a31
s1mpleTEK <57884979+s1mpleTEK@users.noreply.github.com>
2020-08-07 02:54:33 +0200

    Updated the complete chess board to semi-complete chess board

--- Chess_semi.py
+++ Chess_semi.py
@@ -3,12 +3,12 @@
 import numpy as np
 import re
 
-from chess_pieces_move.paw import paw_move, paw_evolution
-from chess_pieces_move.knight import knight_move
-from chess_pieces_move.bishop import bishop_move
-from chess_pieces_move.rook import rook_move
-from chess_pieces_move.queen import queen_move
-from chess_pieces_move.king import king_move
+from chess_pieces_move_semi.paw import paw_move, paw_evolution
+from chess_pieces_move_semi.knight import knight_move
+from chess_pieces_move_semi.bishop import bishop_move
+from chess_pieces_move_semi.rook import rook_move
+from chess_pieces_move_semi.queen import queen_move
+from chess_pieces_move_semi.king import king_move
 
 empty_space = "_"
 corner = "¤"
@@ -25,29 +25,29 @@ def set_user():                                         #set name for players
     return (user)
 
 def set_table():                                    #init game board
-    table = np.zeros((10,10), dtype=str)            #init empty list 10x10 ([9][9])
-    for i in range (1,9):                           #set * in every line
+    table = np.zeros((9,10), dtype=str)            #init empty list 10x10 ([9][9])
+    for i in range (9):                           #set * in every line
         table[i] = empty_space
 
     for i in range (2):                             #i=0 > i=1
-        table[i*9] = corner                         #set ¤ in every corner and browse y of table (first line and last line)
+        table[8] = corner                         #set ¤ in every corner and browse y of table (first line and last line)
         for j in range (1,9):                       #browse x of table
-            table[i*9][j] = area[0][j-1]            #A>B>C>D>E>F>G>H
-        for j in range (1,9):                       #browse y of table and set the number 8 to 1 at [1][0] to [8][0] and [1][9] to [8][9]
-            table[j][i*9] = area[1][-(j-1)-1]       #1>2>3>4>5>6>7>8
+            table[8][j] = area[0][j-1]            #A>B>C>D>E>F>G>H
+        for j in range (8):                       #browse y of table and set the number 8 to 1 at [1][0] to [8][0] and [1][9] to [8][9]
+            table[j][i*9] = area[1][-(j+1)]       #1>2>3>4>5>6>7>8
 
     return (set_pieces(table))
 
 def set_pieces(table):                                              #set all pieces on the board
-    for j in range (1,3):                                           #j equal 1 and 2 // set white pieces when j=1 and set black pieces when j=2
-        for i in range (1, 9):                                      #i equal 1 to 8 // when j=1 edit line[2] and when j=2 edit line[7]
-                table[j**j+j*(j-1)+1][i] = pieces[j-1][0]           #set pawns
-        for i in range (1,3):                                       #i equal 1 and 2 // when j=1 edit line[1] and when j=2 edit line[8]
-                table[j**3][i**3] = pieces[j-1][3]                  #set rooks | i=1 -> i**3 = 1**3 = 1 / i=2 -> i**3 = 2**3 = 8
-                table[j**3][i**i+i*(i-1)+1] = pieces[j-1][1]        #set knights | i=1 -> i**i+i*(i-1)+1 = 1**1+1*(1-1)+1 = 1+1*0+1 = 2 / i=2 -> i**i+i*(i-1)+1 = 2**2+2*(2-1)+1 = 4+2*1+1 = 6+1 = 7
-                table[j**3][i*3] = pieces[j-1][2]                   #set bishops | i=1 -> i*3 = 1*3 = 3 / i=2 -> i*3 = 2*3 = 6
-                table[j**3][4] = pieces[j-1][4]                     #set queen
-                table[j**3][5] = pieces[j-1][5]                     #set king
+    # for j in range (1,3):                                           #j equal 1 and 2 // set white pieces when j=1 and set black pieces when j=2
+    #     for i in range (1, 9):                                      #i equal 1 to 8 // when j=1 edit line[2] and when j=2 edit line[7]
+    #             table[j**j+j*(j-1)+1][i] = pieces[j-1][0]           #set pawns
+    #     for i in range (1,3):                                       #i equal 1 and 2 // when j=1 edit line[1] and when j=2 edit line[8]
+    #             table[j**3][i**3] = pieces[j-1][3]                  #set rooks | i=1 -> i**3 = 1**3 = 1 / i=2 -> i**3 = 2**3 = 8
+    #             table[j**3][i**i+i*(i-1)+1] = pieces[j-1][1]        #set knights | i=1 -> i**i+i*(i-1)+1 = 1**1+1*(1-1)+1 = 1+1*0+1 = 2 / i=2 -> i**i+i*(i-1)+1 = 2**2+2*(2-1)+1 = 4+2*1+1 = 6+1 = 7
+    #             table[j**3][i*3] = pieces[j-1][2]                   #set bishops | i=1 -> i*3 = 1*3 = 3 / i=2 -> i*3 = 2*3 = 6
+    #             table[j**3][4] = pieces[j-1][4]                     #set queen
+    #             table[j**3][5] = pieces[j-1][5]                     #set king
 
     return (table)
 
@@ -153,7 +153,7 @@ def game_condition(table, user):
 def display_table(table):       #display game board
     print("")
 
-    for i in range (10):
+    for i in range (9):
         print(table[i])
 
     print("")
