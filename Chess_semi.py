ac2b99624eabcf3e5d5e232a5293e58dd9d260ee
s1mpleTEK <57884979+s1mpleTEK@users.noreply.github.com>
2020-08-07 03:17:37 +0200

    Updated bishop start position for semi-complete chess board

--- Chess_semi.py
+++ Chess_semi.py
@@ -45,7 +45,7 @@ def set_pieces(table):                                              #set all pie
         for i in range (1,3):                                       #i equal 1 and 2 // when j=1 edit line[1] and when j=2 edit line[8]
                 table[j**3-1][i**3] = pieces[j-1][3]                  #set rooks | i=1 -> i**3 = 1**3 = 1 / i=2 -> i**3 = 2**3 = 8
                 table[j**3-1][i**i+i*(i-1)+1] = pieces[j-1][1]        #set knights | i=1 -> i**i+i*(i-1)+1 = 1**1+1*(1-1)+1 = 1+1*0+1 = 2 / i=2 -> i**i+i*(i-1)+1 = 2**2+2*(2-1)+1 = 4+2*1+1 = 6+1 = 7
-    #             table[j**3-1][i*3] = pieces[j-1][2]                   #set bishops | i=1 -> i*3 = 1*3 = 3 / i=2 -> i*3 = 2*3 = 6
+                table[j**3-1][i*3] = pieces[j-1][2]                   #set bishops | i=1 -> i*3 = 1*3 = 3 / i=2 -> i*3 = 2*3 = 6
     #             table[j**3-1][4] = pieces[j-1][4]                     #set queen
     #             table[j**3-1][5] = pieces[j-1][5]                     #set king
 
