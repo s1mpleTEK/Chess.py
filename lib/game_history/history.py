2f97c960a92cdd99d52aaea53ce098c896f6ef8c
s1mpleTEK <57884979+s1mpleTEK@users.noreply.github.com>
2020-08-20 10:02:37 +0200

    Updated history method: each turn as played writed in file history of this game

--- lib/game_history/history.py
+++ lib/game_history/history.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 import os
-import sys
 import datetime as dt
 
 class History:
-    hist = []
-    turn = 1
+    hist = ""
+    turn_player = 1
+    turn_game = 1
     player1_move = ""
     player2_move = ""
     file_history = ""
@@ -18,18 +18,24 @@ class Os:
 
 def history(table, tmp_table, path_pieces, entity):
     os_operation()
-    if History.turn == 1:
+    if History.turn_player == 1:
         History.player1_move = str(path_pieces[0][0])+"-"+str(path_pieces[1][0])
-        History.turn = 2
+        History.hist = History.player1_move
+        os.write(History.file_history, str.encode(str(History.turn_game) + ". "))
+        os.write(History.file_history, str.encode(History.hist + " "))
+        History.turn_player = 2
     else:
         History.player2_move = str(path_pieces[0][0])+"-"+str(path_pieces[1][0])
-        History.hist.append([History.player1_move, History.player2_move])
-        History.turn = 1
+        History.hist = History.player2_move
+        History.turn_player = 1
+        os.write(History.file_history, str.encode(History.hist))
+        os.write(History.file_history, str.encode("\n"))
+        History.turn_game = History.turn_game + 1
     return
 
 def os_operation():
     if not os.path.exists(Os.path_dirs):
         os.makedirs(Os.path_dirs)
     if not os.path.exists(Os.path_file):
-        History.file_history = os.open(Os.path_file, os.O_CREAT|os.O_APPEND)
+        History.file_history = os.open(Os.path_file, os.O_CREAT|os.O_RDWR)
     return
